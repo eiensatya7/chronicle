4ce9d43b3cffbad5a6a2626da71f9078be8a586c
Sai <eiensatya7@gmail.com>
2024-06-25 23:56:51 +0100

    Adding more html

--- chronicle_src/api_routes.py
+++ chronicle_src/api_routes.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, File, UploadFile, APIRouter
+import pandas as pd
+from numpy import ndarray
+from pandas import DataFrame
+from pandas.core.arrays import ExtensionArray
 
 api_router = APIRouter(prefix="/api/v1")
 
@@ -6,3 +10,11 @@ api_router = APIRouter(prefix="/api/v1")
 @api_router.get("/taskSize")
 def get_task_size():
     return {"message": "Hello taskSize"}
+
+
+@api_router.post("/fetchColumns")
+def create_upload_file(file: UploadFile):
+    df: DataFrame = pd.read_csv(file.file)
+    filtered = [x for x in df.columns.values if 'Unnamed' not in x]
+    print(filtered)
+    return filtered
